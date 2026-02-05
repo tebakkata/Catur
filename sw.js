11c6f55954c30cd116fd83cd2784015e658b7645
tebakkata <mholis1991@gmail.com>
2026-02-05 23:13:43 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,17 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('chess-v1').then(cache =>
+      cache.addAll([
+        './',
+        './index.html',
+        './manifest.json'
+      ])
+    )
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
